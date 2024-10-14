155a710c4370ab96be384732c1b72c160503f619
gabrielbastossobral <gabrielbastossobral12@hotmail.com>
2024-10-14 16:14:12 -0300

    exercise join and substr

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,33 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/10 14:41:18 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/10 14:41:18 by marvin           ###   ########.fr       */
+/*   Created: 2024/10/14 11:40:22 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/14 11:40:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strdup(const char *s1)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
 	size_t	i;
+	size_t	j;
 
-	if (!s1)
-		return (NULL);
-	str = (char*)malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
+	str = (char*)malloc(
+		sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
+	j = 0;
 	while (s1[i])
 	{
-		str[i] = s1[i];
+		str[j++] = s1[i];
 		i++;
 	}
-	str[i] = 0;
+	i = 0;
+	while (s2[i])
+	{
+		str[j++] = s2[i];
+		i++;
+	}
+	str[j] = 0;
 	return (str);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -1,33 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/10 14:41:18 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/10 14:41:18 by marvin           ###   ########.fr       */
+/*   Created: 2024/10/14 11:07:15 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/14 11:07:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strdup(const char *s1)
+char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
 	size_t	i;
+	size_t	j;
+	char	*str;
 
-	if (!s1)
-		return (NULL);
-	str = (char*)malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
+	str = (char*)malloc(sizeof(*s) * (len + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	j = 0;
+	while (s[i])
 	{
-		str[i] = s1[i];
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
 		i++;
 	}
-	str[i] = 0;
+	str[j] = 0;
 	return (str);
 }
